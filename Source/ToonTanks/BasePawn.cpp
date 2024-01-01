30a40685ec2267f7a6f910ff57054ab082747998
Maciej Sliwinski <maciej.sliwinski98@gmail.com>
2024-01-01 18:05:22 +0100

    Tower class

--- Source/ToonTanks/BasePawn.cpp
+++ Source/ToonTanks/BasePawn.cpp
@@ -3,6 +3,7 @@
 
 #include "BasePawn.h"
 #include "Components/CapsuleComponent.h"
+#include "Kismet/GameplayStatics.h"
 
 // Sets default values
 ABasePawn::ABasePawn()
@@ -29,6 +30,10 @@ void ABasePawn::RotateTurret(FVector LookAtTarget)
 	FVector ToTarget = LookAtTarget - TurretMesh->GetComponentLocation();
 	FRotator LookAtRotation = FRotator(0.f,ToTarget.Rotation().Yaw,0.f);
 
-	TurretMesh->SetWorldRotation(LookAtRotation);
+	TurretMesh->SetWorldRotation(
+		FMath::RInterpTo(TurretMesh->GetComponentRotation(), 
+		LookAtRotation, 
+		UGameplayStatics::GetWorldDeltaSeconds(this), 
+		35.f));
 	
 }
